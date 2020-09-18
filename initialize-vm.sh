a4b5c42430ed3a52cd94082a9fcab8532a155240
Matheus Elyasha <matheuselyasha@gmail.com>
2020-09-18 23:03:04 +0200

    Added initialize-vm shell script

---
+++ initialize-vm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VM_NUMBER = $vm_number
+
+cd 
+git clone https://github.com/gobrasa/docker-jitsi-meet.git
+cd docker-jitsi-meet
+cp envs/.env$VM_NUMBER .env
+./gen-passwords.sh
+mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+docker-compose up -d
+
